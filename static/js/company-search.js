e62db66cfc9dc8f83ef23e92027e42813e88fc2a
Benjamin Altpeter <benjamin@oldpeter.de>
2018-03-19 04:29:55 +0100

    Implemented (rudimentary) company search

---
+++ static/js/company-search.js
@@ -0,0 +1,17 @@
+var client = algoliasearch('M90RBUHW3U', 'a306a2fc33ccc9aaf8cbe34948cf97ed');
+var index = client.initIndex('companies');
+
+autocomplete('#aa-search-input', { hint: false, debug: true }, {
+    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+    displayKey: 'name',
+    templates: {
+        suggestion: function(suggestion) {
+            return '<span><strong>' + suggestion._highlightResult.name.value + '</strong></span>'
+                + (suggestion._highlightResult.runs ? '<br><span>' + suggestion._highlightResult.runs.map(e => e.value).join(', ') + '</span>' : '')
+                + (suggestion._highlightResult.categories ? '<br><span>' + suggestion._highlightResult.categories.map(e => e.value).join(', ') + '</span>' : '');
+        }
+    }
+})
+    .on('autocomplete:selected', function(event, suggestion, dataset) {
+        location.href = '/company?slug=' + suggestion.slug;
+    });
