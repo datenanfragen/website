12970f90c137b8bd08e2d8aabb79602be32cdd10
Benjamin Altpeter <benjamin@oldpeter.de>
2018-03-28 02:08:30 +0200

    WIP: Started moving to (p)react :'(

---
+++ src/generator.js
@@ -0,0 +1,3 @@
+import preact from 'preact';
+
+preact.render((<h1>Demo</h1>), document.body);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/generator.js',
+    output: {
+        filename: 'generator.gen.js',
+        path: path.resolve(__dirname, 'static/js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
