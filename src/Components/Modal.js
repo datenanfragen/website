f9474e3786bef1f817800abc5795cd467e63510d
Lorenz Sieben <me@lorenz-sieben.com>
2018-10-07 21:47:31 +0000

    Fixes #98,#119: Improved modal flow

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,14 +1,16 @@
 import preact from 'preact';
 import Portal from 'preact-portal';
+import t from "../Utility/i18n";
 
 export default class Modal extends preact.Component {
     render() {
-        let positiveButton = this.props.positiveText ? <button className={this.props.positiveDefault ? 'button-primary' : ''} onClick={this.props.onPositiveFeedback} style={"float: left"}>{this.props.positiveText}</button> : '';
-        let negativeButton = this.props.negativeText ? <button className={!this.props.positiveDefault ? 'button-primary' : ''} onClick={this.props.onNegativeFeedback} style={"float: right"}>{this.props.negativeText}</button> : '';
+        let positiveButton = this.props.positiveText ? <button className={this.props.positiveDefault ? 'button-primary' : ''} onClick={this.props.onPositiveFeedback} style={"float: right"}>{this.props.positiveText}</button> : '';
+        let negativeButton = this.props.negativeText ? <button className={!this.props.positiveDefault ? 'button-primary' : ''} onClick={this.props.onNegativeFeedback} style={"float: left"}>{this.props.negativeText}</button> : '';
         return <Portal into="body">
             <div className='modal'>
                 <div className="backdrop" onClick={this.props.onDismiss} />
                 <div className="inner">
+                    {!!this.props.onDismiss ? <a className="close-button icon-close" onClick={this.props.onDismiss} title={t('cancel', 'generator')} /> : [] }
                     {this.props.children}
                     <div className="button-group">
                         {positiveButton}
