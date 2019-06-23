f278d5f73598cdd2c1622279159826b115dc6912
Benjamin Altpeter <benjamin@oldpeter.de>
2019-06-23 17:43:46 +0000

    Apply suggestion to src/general.js

--- src/general.js
+++ src/general.js
@@ -75,7 +75,7 @@ function guessUserCountry() {
 
     // taken from https://github.com/gagle/node-bcp47/blob/master/lib/index.js#L4
     const bcp47_regex = /^(?:(en-gb-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-be-fr|sgn-be-nl|sgn-ch-de)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))$|^((?:[a-z]{2,3}(?:(?:-[a-z]{3}){1,3})?)|[a-z]{4}|[a-z]{5,8})(?:-([a-z]{4}))?(?:-([a-z]{2}|\d{3}))?((?:-(?:[\da-z]{5,8}|\d[\da-z]{3}))*)?((?:-[\da-wyz](?:-[\da-z]{2,8})+)*)?(-x(?:-[\da-z]{1,8})+)?$|^(x(?:-[\da-z]{1,8})+)$/i;
-    let bcp47_country = bcp47_regex.exec(navigator_lang)[5];
+    let bcp47_country = bcp47_regex.exec(navigator_lang)[5].toLowerCase();
 
     // If however we *can* guess the country but just don't support it, we show all companies.
     return bcp47_country && SUPPORTED_COUNTRIES.includes(bcp47_country) ? bcp47_country : 'all';
