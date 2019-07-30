ef255f8c492005f0579366d74afa3bb4d4b93692
Benjamin Altpeter <benjamin@oldpeter.de>
2019-07-30 21:15:58 +0200

    Make RequestTypeChooser more flexible
    * Allow restricting the available types.
    * For <=2 types, the radio group can be horizontal.
    * If no `current` is specified, use the first one.

--- src/Components/RequestTypeChooser.js
+++ src/Components/RequestTypeChooser.js
@@ -2,47 +2,41 @@ import preact from 'preact';
 import t from 'Utility/i18n';
 import Radio from './Radio';
 
+const VALID_REQUEST_TYPES = ['access', 'erasure', 'rectification', 'custom'];
+
 export default class RequestTypeChooser extends preact.Component {
     render() {
+        const request_types = this.props.request_types.filter(type => VALID_REQUEST_TYPES.includes(type));
+        const current = this.props.current || request_types[0];
+        const radios = request_types.map(type => (
+            <Radio
+                id={'request-type-choice-' + type}
+                radio_variable={current}
+                value={type}
+                name="request-type"
+                onChange={this.props.onTypeChange}
+                label={t(type + '-request', 'generator')}
+            />
+        ));
+
         return (
             <div className="request-type-chooser">
                 {t('request-type', 'generator')}
                 <br />
-                <div className="radio-group radio-group-vertical">
-                    <Radio
-                        id="request-type-choice-access"
-                        radio_variable={this.props.current}
-                        value="access"
-                        name="request-type"
-                        onChange={this.props.onTypeChange}
-                        label={t('access-request', 'generator')}
-                    />
-                    <Radio
-                        id="request-type-choice-erasure"
-                        radio_variable={this.props.current}
-                        value="erasure"
-                        name="request-type"
-                        onChange={this.props.onTypeChange}
-                        label={t('erasure-request', 'generator')}
-                    />
-                    <Radio
-                        id="request-type-choice-rectification"
-                        radio_variable={this.props.current}
-                        value="rectification"
-                        name="request-type"
-                        onChange={this.props.onTypeChange}
-                        label={t('rectification-request', 'generator')}
-                    />
-                    <Radio
-                        id="request-type-choice-custom"
-                        radio_variable={this.props.current}
-                        value="custom"
-                        name="request-type"
-                        onChange={this.props.onTypeChange}
-                        label={t('own-request', 'generator')}
-                    />
+                <div
+                    className={
+                        'radio-group' + (request_types.length > 2 ? ' radio-group-vertical' : 'radio-group-horizontal')
+                    }>
+                    {radios}
                 </div>
             </div>
         );
+    }
+
+    static get defaultProps() {
+        return {
+            current: null,
+            request_types: VALID_REQUEST_TYPES
+        };
     }
 }
