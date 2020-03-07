4a87562bdc0b6f4aba681d8c9f249930397a6d02
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-03-07 15:55:36 +0100

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,62 @@
 #!/usr/bin/env bash
 set -e
 
-# --- Fetch companies data ---
+echo "--- Fetching data ---"
 git clone --depth 1 https://github.com/datenanfragen/data data_tmp
-mkdir -p content/company
-mkdir -p content/supervisory-authority
+
+echo "--- Creating directories ---"
+for lang in de en fr
+do
+mkdir -p "content/$lang/company"
+mkdir -p "content/$lang/supervisory-authority"
+done
+
 mkdir -p static/templates
 mkdir -p static/db
 mkdir -p static/db/suggested-companies
 mkdir -p static/db/sva
+
+echo "--- Copying files ---"
 cp data_tmp/companies/* static/db
 cp data_tmp/suggested-companies/* static/db/suggested-companies
 cp data_tmp/supervisory-authorities/* static/db/sva
-cp data_tmp/companies/* content/company
-cp data_tmp/supervisory-authorities/* content/supervisory-authority
+
+for lang in de en fr
+do
+cp data_tmp/companies/* "content/$lang/company"
+cp data_tmp/supervisory-authorities/* "content/$lang/supervisory-authority"
+done
+
 cp -r data_tmp/templates/* static/templates
+
 mv data_tmp/schema.json data_tmp/schema-supervisory-authorities.json static
+
 rm -rf data_tmp
+
 node prepare-deploy.js
+
+cd content
+
 # Unfortunately, Hugo only accepts .md files as posts, so we have to rename our JSONs, see https://stackoverflow.com/a/27285610
-cd content/company || exit
-# Hugo doesn't fallback to the default language here, so we have to copy the files for every language
-find . -name '*.json' -exec sh -c 'for lang in de fr pt ; do cp "$0" "${0%.json}.$lang.md" ; done && mv "$0" "${0%.json}.en.md"' {} \;
+echo "--- Renaming JSON files ---"
 
-cd ../supervisory-authority || exit
-find . -name '*.json' -exec sh -c 'for lang in de fr pt ; do cp "$0" "${0%.json}.$lang.md" ; done && mv "$0" "${0%.json}.en.md"' {} \;
+for lang in de en fr
+do
+	find "$lang/company" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
+	find "$lang/supervisory-authority" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
+done
 
-cd ../.. || exit
+cd .. || exit
 
 yarn licenses generate-disclaimer --ignore-optional --ignore-platform > static/NOTICES.txt
 
-# --- Run Webpack and Hugo ---
+echo "--- Running Webpack and Hugo ---"
 yarn run build
 
 if [ "$CONTEXT" = "production" ]
 then
 	hugo -e production --minify
-	# Copy files for Netlify
+	echo "--- Copying files for Netlify ---"
 	cp _redirects public/_redirects
 	cp static/404.html public/404.html
 else
