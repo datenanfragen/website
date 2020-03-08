797f0e9afce24fec6652da4e8aa74617fea1660e
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-03-08 19:03:42 +0100

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-echo "--- Fetching data ---"
+languages=(de en fr)
+
+echo "Fetching data…"
 git clone --depth 1 https://github.com/datenanfragen/data data_tmp
 
-echo "--- Creating directories ---"
-for lang in de en fr
+echo "Creating directories…"
+for lang in ${languages[@]}
 do
 mkdir -p "content/$lang/company"
 mkdir -p "content/$lang/supervisory-authority"
@@ -16,12 +18,12 @@ mkdir -p static/db
 mkdir -p static/db/suggested-companies
 mkdir -p static/db/sva
 
-echo "--- Copying files ---"
+echo "Copying files…"
 cp data_tmp/companies/* static/db
 cp data_tmp/suggested-companies/* static/db/suggested-companies
 cp data_tmp/supervisory-authorities/* static/db/sva
 
-for lang in de en fr
+for lang in ${languages[@]}
 do
 cp data_tmp/companies/* "content/$lang/company"
 cp data_tmp/supervisory-authorities/* "content/$lang/supervisory-authority"
@@ -35,12 +37,12 @@ rm -rf data_tmp
 
 node prepare-deploy.js
 
-cd content
+cd content || exit
 
 # Unfortunately, Hugo only accepts .md files as posts, so we have to rename our JSONs, see https://stackoverflow.com/a/27285610
-echo "--- Renaming JSON files ---"
+echo "Renaming JSON files…"
 
-for lang in de en fr
+for lang in ${languages[@]}
 do
 	find "$lang/company" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
 	find "$lang/supervisory-authority" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
@@ -50,13 +52,13 @@ cd .. || exit
 
 yarn licenses generate-disclaimer --ignore-optional --ignore-platform > static/NOTICES.txt
 
-echo "--- Running Webpack and Hugo ---"
+echo "Running Webpack and Hugo…"
 yarn run build
 
 if [ "$CONTEXT" = "production" ]
 then
 	hugo -e production --minify
-	echo "--- Copying files for Netlify ---"
+	echo "Copying files for Netlify…"
 	cp _redirects public/_redirects
 	cp static/404.html public/404.html
 else
