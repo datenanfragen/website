922754f310d39c78466dfb161518452731a7e69d
Lorenz Sieben <me@lorenz-sieben.com>
2020-08-25 18:43:25 +0200

    Fix postcss problems and use postcss-url

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,22 @@ module.exports = (ctx) => {
                 browsers: ['last 2 versions', '> 5%'],
             }),
             require('cssnano')({}),
+            require('postcss-url')([
+                {
+                    filter: /\.svg/,
+                    basePath: path.resolve(__dirname, 'assets/'),
+                    url: 'inline',
+                    optimizeSvgEncode: true,
+                },
+                {
+                    filter: /\.(png|jpg|gif|eot|ttf|woff|woff2)$/,
+                    basePath: path.resolve(__dirname, 'assets/'),
+                    maxSize: 10,
+                    fallback: 'copy',
+                    assetsPath: path.resolve(__dirname, 'public/img'),
+                    useHash: true,
+                },
+            ]),
         ],
     };
 };
