6ee522b10bdf160517a44238454dd7aa45c606a9
Benjamin Altpeter <benjamin@oldpeter.de>
2020-09-01 21:18:22 +0200

    Fixes #388: Only load saved signature if there is one

--- src/Components/RequestGeneratorBuilder.js
+++ src/Components/RequestGeneratorBuilder.js
@@ -34,21 +34,21 @@ export default class RequestGeneratorBuilder extends preact.Component {
             response_request: {},
 
             fill_fields: [],
-            fill_signature: null
+            fill_signature: null,
         };
 
         this.letter = new RequestLetter({}, (blob_url, filename) => {
             this.setState({
                 blob_url: blob_url,
                 download_filename: filename,
-                download_active: true
+                download_active: true,
             });
         });
 
         if (Privacy.isAllowed(PRIVACY_ACTIONS.SAVE_ID_DATA)) {
             this.savedIdData = new SavedIdData();
-            this.savedIdData.getAll(false).then(fill_fields => this.setState({ fill_fields: fill_fields }));
-            this.savedIdData.getSignature().then(fill_signature => this.setState({ fill_signature: fill_signature }));
+            this.savedIdData.getAll(false).then((fill_fields) => this.setState({ fill_fields: fill_fields }));
+            this.savedIdData.getSignature().then((fill_signature) => this.setState({ fill_signature: fill_signature }));
         }
 
         this.resetInitialConditions().then(() => {
@@ -79,15 +79,17 @@ export default class RequestGeneratorBuilder extends preact.Component {
     }
 
     resetInitialConditions = async () => {
-        await initializeFields(this.state.request.id_data).then(res => {
-            this.setState(prev => {
-                prev.request.id_data = res.new_fields;
-                prev.request.signature = res.signature;
+        await initializeFields(this.state.request.id_data).then((res) => {
+            this.setState((prev) => {
+                if (res.new_fields) {
+                    prev.request.id_data = res.new_fields;
 
-                const name = res.new_fields.filter(i => i.type === 'name')[0];
-                const address = res.new_fields.filter(i => i.type === 'address')[0];
-                if (name) prev.request.custom_data.name = name.value;
-                if (address) prev.request.custom_data.sender_address = address.value;
+                    const name = res.new_fields.filter((i) => i.type === 'name')[0];
+                    const address = res.new_fields.filter((i) => i.type === 'address')[0];
+                    if (name) prev.request.custom_data.name = name.value;
+                    if (address) prev.request.custom_data.sender_address = address.value;
+                }
+                if (res.signature) prev.request.signature = res.signature;
 
                 return prev;
             });
@@ -98,22 +100,22 @@ export default class RequestGeneratorBuilder extends preact.Component {
         // This is a response to a previous request (warning or complaint).
         if (Privacy.isAllowed(PRIVACY_ACTIONS.SAVE_MY_REQUESTS) && response_to && response_type) {
             // Just for the looks: Switch the view before fetching the template
-            this.setState(prev => {
+            this.setState((prev) => {
                 prev.request.type = 'custom';
                 return prev;
             });
 
             await Promise.all([
                 new UserRequests().getRequest(response_to),
-                fetchTemplate(this.state.request.language, response_type, null, '')
-            ]).then(results => {
+                fetchTemplate(this.state.request.language, response_type, null, ''),
+            ]).then((results) => {
                 const [request, text] = results;
 
-                this.setState(prev => {
+                this.setState((prev) => {
                     prev.request.custom_data.content = new Template(text, [], {
                         request_article: REQUEST_ARTICLES[request.type],
                         request_date: request.date,
-                        request_recipient_address: request.recipient
+                        request_recipient_address: request.recipient,
                     }).getText();
 
                     if (response_type === 'admonition') {
@@ -138,7 +140,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
         }
         // This is just a regular ol' request.
         else {
-            await fetchTemplate(this.state.request.language, 'access').then(text => {
+            await fetchTemplate(this.state.request.language, 'access').then((text) => {
                 this.setState({ template_text: text });
             });
         }
@@ -147,10 +149,10 @@ export default class RequestGeneratorBuilder extends preact.Component {
     componentDidMount() {
         if (Privacy.isAllowed(PRIVACY_ACTIONS.SAVE_ID_DATA)) {
             const callback = () => {
-                this.savedIdData.getAll(false).then(fill_fields => this.setState({ fill_fields: fill_fields }));
+                this.savedIdData.getAll(false).then((fill_fields) => this.setState({ fill_fields: fill_fields }));
                 this.savedIdData
                     .getSignature()
-                    .then(fill_signature => this.setState({ fill_signature: fill_signature }));
+                    .then((fill_signature) => this.setState({ fill_signature: fill_signature }));
             };
 
             window.addEventListener(ID_DATA_CHANGE_EVENT, callback);
@@ -160,7 +162,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
 
     render() {
         const replacers = replacer_factory(this);
-        const children_mapper = c =>
+        const children_mapper = (c) =>
             c.nodeName && Object.keys(replacers).includes(c.nodeName.name)
                 ? replacers[c.nodeName.name](c)
                 : Array.isArray(c.children)
@@ -175,20 +177,20 @@ export default class RequestGeneratorBuilder extends preact.Component {
         );
     }
 
-    startBatch = companies => {
+    startBatch = (companies) => {
         this.setState({ batch: companies });
         if (this.state.batch?.length > 0) this.setCompanyBySlug(this.state.batch.shift());
     };
 
-    setCompanyBySlug = async slug => {
-        await fetchCompanyDataBySlug(slug).then(company => {
+    setCompanyBySlug = async (slug) => {
+        await fetchCompanyDataBySlug(slug).then((company) => {
             this.setCompany(company);
         });
     };
 
-    setCompany = company => {
+    setCompany = (company) => {
         if (this.state.request.type !== 'custom') {
-            fetchTemplate(company['request-language'], this.state.request.type, company).then(text => {
+            fetchTemplate(company['request-language'], this.state.request.type, company).then((text) => {
                 this.setState({ template_text: text });
                 this.renderLetter();
             });
@@ -196,7 +198,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
 
         // I would love to have `Request` handle this. But unfortunately that won't work as Preact won't notice the
         // state has changed. :(
-        this.setState(prev => {
+        this.setState((prev) => {
             prev.request.transport_medium = company['suggested-transport-medium']
                 ? company['suggested-transport-medium']
                 : company.email
@@ -230,7 +232,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
                 'access-tracking',
                 'erasure-tracking',
                 'rectification-tracking',
-                'objection-tracking'
+                'objection-tracking',
             ].includes(company['custom-' + this.state.request.type + '-template'] || '');
 
             const intermediate_id_data = SavedIdData.mergeFields(
@@ -262,21 +264,21 @@ export default class RequestGeneratorBuilder extends preact.Component {
         });
     };
 
-    handleTypeChange = e => {
+    handleTypeChange = (e) => {
         this.handleInputChange({ type: e.target.value });
         if (e.target.value === 'custom') {
             this.letter.clearProps();
             return;
         }
 
-        fetchTemplate(this.state.request.language, this.state.request.type, this.state.suggestion).then(text => {
+        fetchTemplate(this.state.request.language, this.state.request.type, this.state.suggestion).then((text) => {
             this.setState({ template_text: text });
             this.renderLetter();
         });
     };
 
-    handleInputChange = changed_data => {
-        this.setState(prev => {
+    handleInputChange = (changed_data) => {
+        this.setState((prev) => {
             for (const key in changed_data) {
                 prev.request[key] = changed_data[key];
             }
@@ -286,10 +288,10 @@ export default class RequestGeneratorBuilder extends preact.Component {
         this.renderLetter();
     };
 
-    handleTransportMediumChange = e => {
+    handleTransportMediumChange = (e) => {
         const by_fax_text = t_r('by-fax', this.state.request.language);
 
-        this.setState(prev => {
+        this.setState((prev) => {
             prev.request.transport_medium = e.target.value;
             switch (e.target.value) {
                 case 'fax':
@@ -315,13 +317,13 @@ export default class RequestGeneratorBuilder extends preact.Component {
 
     handleCustomLetterPropertyChange = (e, address_change = false) => {
         if (address_change) {
-            this.setState(prev => {
+            this.setState((prev) => {
                 const att = e.target.getAttribute('name');
                 prev.request.custom_data.sender_address[att] = e.target.value;
                 return prev;
             });
         } else {
-            this.setState(prev => {
+            this.setState((prev) => {
                 const att = e.target.getAttribute('name');
                 if (Object.prototype.hasOwnProperty.call(prev.request.custom_data, att))
                     prev.request.custom_data[att] = e.target.value;
@@ -331,11 +333,11 @@ export default class RequestGeneratorBuilder extends preact.Component {
         this.renderLetter();
     };
 
-    handleCustomLetterTemplateChange = e => {
+    handleCustomLetterTemplateChange = (e) => {
         const new_template = e.target.value;
         if (new_template !== 'no-template') {
-            fetchTemplate(this.state.request.language, new_template, null, '').then(text => {
-                this.setState(prev => {
+            fetchTemplate(this.state.request.language, new_template, null, '').then((text) => {
+                this.setState((prev) => {
                     prev.request.custom_data.content = text;
                     prev.request.response_type = new_template;
                     return prev;
@@ -343,7 +345,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
                 this.renderLetter();
             });
         } else
-            this.setState(prev => {
+            this.setState((prev) => {
                 prev.request.response_type = '';
                 return;
             });
@@ -355,12 +357,12 @@ export default class RequestGeneratorBuilder extends preact.Component {
                 <Modal
                     positiveText={t('new-request', 'generator')}
                     negativeText={t('override-request', 'generator')}
-                    onNegativeFeedback={e => {
+                    onNegativeFeedback={(e) => {
                         dismissModal(modal);
                         this.setCompany(suggestion.document);
                         this.renderLetter();
                     }}
-                    onPositiveFeedback={e => {
+                    onPositiveFeedback={(e) => {
                         dismissModal(modal);
                         this.newRequest().then(() => {
                             this.setCompany(suggestion.document);
@@ -389,14 +391,14 @@ export default class RequestGeneratorBuilder extends preact.Component {
                     <MarkupText id="modal-select-authority" />
                 </IntlProvider>
                 <SvaFinder
-                    callback={sva => {
+                    callback={(sva) => {
                         this.setCompany(sva);
-                        fetchTemplate(sva['complaint-language'], 'complaint', null, '').then(text => {
-                            this.setState(prev => {
+                        fetchTemplate(sva['complaint-language'], 'complaint', null, '').then((text) => {
+                            this.setState((prev) => {
                                 prev.request.custom_data.content = new Template(text, [], {
                                     request_article: REQUEST_ARTICLES[this.state.response_request.type],
                                     request_date: this.state.response_request.date,
-                                    request_recipient_address: this.state.response_request.recipient
+                                    request_recipient_address: this.state.response_request.recipient,
                                 }).getText();
                             });
                             this.renderLetter();
@@ -416,7 +418,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
             sender.street_1,
             sender.street_2,
             sender.place,
-            sender.country
+            sender.country,
         ];
         if (this.state.request.type === 'custom') {
             this.letter.setProps({
@@ -426,7 +428,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
                 recipient_address: this.state.request.recipient_address,
                 sender_address: sender_address,
                 information_block: RequestLetter.makeInformationBlock(this.state.request),
-                reference_barcode: RequestLetter.barcodeFromText(this.state.request.reference)
+                reference_barcode: RequestLetter.barcodeFromText(this.state.request.reference),
             });
         } else this.letter.setProps(RequestLetter.propsFromRequest(this.state.request, this.state.template_text));
 
@@ -444,9 +446,9 @@ export default class RequestGeneratorBuilder extends preact.Component {
                 this.setState({
                     blob_url: URL.createObjectURL(
                         new Blob([this.letter.toEmailString(true)], {
-                            type: 'text/plain'
+                            type: 'text/plain',
                         })
-                    )
+                    ),
                 });
                 break;
             }
@@ -462,7 +464,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
 
         if (this.props.newRequestHook) this.props.newRequestHook(this);
 
-        this.setState(prev => {
+        this.setState((prev) => {
             prev.request = new Request();
             prev.request.done = false;
             prev.suggestion = null;
@@ -507,7 +509,7 @@ export default class RequestGeneratorBuilder extends preact.Component {
                     </div>
                 }
                 negativeText={t('new-request', 'generator')}
-                onNegativeFeedback={e => {
+                onNegativeFeedback={(e) => {
                     dismissModal(modal);
                     this.newRequest().then(() => {
                         // We are in batch mode, move to the next company.
