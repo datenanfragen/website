datenanfragen/website

commit 8110065039adb5fee4b722c71a26a02607635858

Applied link styling by modifying link selector. (#426)
* Applied link styling by modifying link selector.

* Modified link decoration behaviour.

* Modified link decoration behaviour.

layouts/supervisory-authority/single.html
@@ -9,7 +9,7 @@
 {{ define "main" }}
 <main class="narrow-page" style="overflow-wrap: break-word;">
     <div style="display: flex; align-items: center; justify-content: space-between;">
-        <a href="{{ "supervisory-authority" | absURL }}"><span class="icon inline-icon inline-icon-left icon-arrow-left"></span> {{ T "cdb-back-to-overview" }}</a>
+        <a class="no-link-decoration" href="{{ "supervisory-authority" | absURL }}"><span class="icon inline-icon inline-icon-left icon-arrow-left"></span> {{ T "cdb-back-to-overview" }}</a>
         <span>
             <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.{{ .Lang }}" title="{{ T "cdb-cc0-explanation" }}"><img src="{{ "img/cc-zero-button.svg" | absURL }}"></a>
             <a href="{{ "db/sva" | absURL }}/{{ .Params.slug }}.json" title="{{ T "cdb-json-download" }}"><span class="icon inline-icon inline-icon-right icon-json-document" style="color: initial;"></a>

layouts/company/single.html
@@ -9,7 +9,7 @@
 {{ define "main" }}
 <main class="narrow-page" style="overflow-wrap: break-word;">
     <div style="display: flex; align-items: center; justify-content: space-between;">
-        <a href="{{ "company" | absURL }}"><span class="icon inline-icon inline-icon-left icon-arrow-left"></span>
+        <a class="no-link-decoration" href="{{ "company" | absURL }}"><span class="icon inline-icon inline-icon-left icon-arrow-left"></span>
             {{ T "cdb-back-to-overview" }}</a>
         <span>
             <a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/deed.{{ .Lang }}"
@@ -66,7 +66,7 @@
         {{ $title := . }}
         <!-- Yes, that is actually the *recommended* way to do this (https://gohugo.io/templates/taxonomy-templates/#example-list-tags-in-a-single-page-template)… *sigh* -->
         {{ with $.Site.GetPage (printf "/categories/%s" (urlize .)) }}
-        {{ $.Scratch.Add "categories" (printf "<a href='%s' class='no-link-decoration'>%s</a>" .Permalink (T $title)) }}
+        {{ $.Scratch.Add "categories" (printf "<a href='%s'>%s</a>" .Permalink (T $title)) }}
         {{ end }}
         {{ end }}
         {{ with .Params.categories }}<p><strong>{{ T "cdb-categories" (len .) }}</strong>

assets/styles/framework.scss
@@ -194,10 +194,8 @@ a:focus {
     color: color('teal', 800);
 }
 
-p > a,
-span > a,
-.box a {
-    &:not(.button):not(.no-link-decoration) {
+main a {
+    &:not(.button):not(.no-link-decoration):not(.icon) {
         position: relative;
         text-decoration: none;
         @media (prefers-reduced-motion: no-preference) {

layouts/partials/summary.html
@@ -1,7 +1,7 @@
 <article class="list-article">
     {{ if (or .Params.featured_image .Params.featured_image_url) }}
         <div class="col25 article-featured-image">
-            <a href="{{ .Permalink }}">
+            <a href="{{ .Permalink }}" class="no-link-decoration">
                 {{ if .Params.featured_image }}
                     <img srcset="
                             {{- partial "functions/imgURL" (dict "page" .Page "name" .Params.featured_image "command" "Resize" "options" "280x") }} 280w,

layouts/shortcodes/img.html
@@ -24,7 +24,7 @@ Inspired by: https://gohugo.io/templates/shortcode-templates/#single-named-examp
 {{- $alt_text := cond (isset .Params "alt") (.Get "alt") (.Get "caption") -}}
 
 <figure{{ with .Get "class" }} class="{{ . }}"{{ end }}>
-    {{ with .Get "link" }}<a href="{{ . }}">{{ end -}}
+    {{ with .Get "link" }}<a class="no-link-decoration" href="{{ . }}">{{ end -}}
     {{- if .Get "name" -}}
         <!--
         An explanation of the sizes and breakpoints used:

commit 68b1f91c98ef6b55411cacf74b7dd622738ccb14

Fix various link styling regressions from #426

layouts/partials/embedded-video.html
@@ -1,4 +1,4 @@
 <div class="embed-container">
     <iframe width="1920" height="1080" class="event-video" src="{{ .url }}"></iframe>
-    <a class="embed-yourself-link" href="{{ .url }}"><span class="icon-code"></span>&nbsp;&nbsp;{{ T "video-embed-yourself" }}</a>
+    <a class="embed-yourself-link no-link-decoration" href="{{ .url }}"><span class="icon-code"></span>&nbsp;&nbsp;{{ T "video-embed-yourself" }}</a>
 </div>

layouts/partials/summary-company.html
@@ -5,7 +5,7 @@
 {{ end }}
 
 <article class="company-article company-summary">
-	<h1><a href="{{ .Permalink }}">{{ .Params.name }}</a></h1>
+	<h1><a href="{{ .Permalink }}" class="no-link-decoration">{{ .Params.name }}</a></h1>
 	{{ with .Params.categories }}<div><strong>{{ T "cdb-categories" (len .) }}</strong> {{ delimit (sort ($.Scratch.Get "categories")) ", " }}</div>{{ end }}
 	{{ with .Params.runs }}<div><strong>{{ T "cdb-runs" }}</strong> {{ delimit . ", " }}</div>{{ end }}
 </article>

layouts/partials/summary-sva.html
@@ -1,5 +1,5 @@
 <article class="company-article company-summary" style="margin-bottom: 50px;">
-	<h1><a href="{{ .Permalink }}">{{ .Params.name }}</a></h1>
+	<h1><a href="{{ .Permalink }}" class="no-link-decoration">{{ .Params.name }}</a></h1>
 	{{ with .Params.address}}
         <div class="col40 col100-mobile"><strong>{{ T "cdb-address" }}</strong><br>{{ replace . "\n" "<br>" | safeHTML }}</div>
     {{ end }}

layouts/supervisory-authority/single.html
@@ -11,8 +11,8 @@
     <div style="display: flex; align-items: center; justify-content: space-between;">
         <a class="no-link-decoration" href="{{ "supervisory-authority" | absURL }}"><span class="icon inline-icon inline-icon-left icon-arrow-left"></span> {{ T "cdb-back-to-overview" }}</a>
         <span>
-            <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.{{ .Lang }}" title="{{ T "cdb-cc0-explanation" }}"><img src="{{ "img/cc-zero-button.svg" | absURL }}"></a>
-            <a href="{{ "db/sva" | absURL }}/{{ .Params.slug }}.json" title="{{ T "cdb-json-download" }}"><span class="icon inline-icon inline-icon-right icon-json-document" style="color: initial;"></a>
+            <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.{{ .Lang }}" title="{{ T "cdb-cc0-explanation" }}" class="no-link-decoration"><img src="{{ "img/cc-zero-button.svg" | absURL }}"></a>
+            <a href="{{ "db/sva" | absURL }}/{{ .Params.slug }}.json" title="{{ T "cdb-json-download" }}" class="no-link-decoration"><span class="icon inline-icon inline-icon-right icon-json-document" style="color: initial;"></a>
         </span>
     </div>
 

src/company-list.js
@@ -37,14 +37,16 @@ class CompanyList extends preact.Component {
             ['W', 'W'],
             ['X', 'X'],
             ['Y', 'Y'],
-            ['Z', 'Z']
+            ['Z', 'Z'],
         ]);
         let anchor_links = [];
         let anchor_ids = [];
         anchor_map.forEach((value, key, map) => {
             anchor_links.push(
                 <li>
-                    <a href={'#' + value}>{key}</a>
+                    <a href={'#' + value} className="no-link-decoration">
+                        {key}
+                    </a>
                 </li>
             );
             anchor_ids.push(value + '-container');