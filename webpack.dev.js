8df64a62cda2c84a7d0a64ce3f6677091678b780
Niharikajain2812 <niharikajainmiet@gmail.com>
2020-10-16 13:32:28 +0530

    Update webpack.dev.js
    const common = require('./webpack.common.js');

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     devtool: 'eval-source-map',
