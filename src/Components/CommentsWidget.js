a2ad618afcddd0d1e972cecf1e2fe1428d13a7f2
AdnCodez <adnan@adncodez.com>
2020-10-17 17:14:52 +0100

    Fixed issue #448, stars no longer interfere with username, using ccs grid

--- src/Components/CommentsWidget.js
+++ src/Components/CommentsWidget.js
@@ -72,7 +72,7 @@ export class Comment extends preact.Component {
                     <strong>{this.props.author}</strong> ({new Date(this.props.date).toLocaleString()})
                 </span>
                 {this.props.additional.rating ? (
-                    <div style="position: absolute; top: 0; right: 0; margin-top: 5px;">
+                    <div className="stars-wrapper">
                         <StarWidget
                             id={'stars-' + this.props.id}
                             initial={this.props.additional.rating}
