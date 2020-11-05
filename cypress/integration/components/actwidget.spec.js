3343a94f030c7b6a75e10f2d2d8dde8a572d6131
mal-tee <45886910+mal-tee@users.noreply.github.com>
2020-11-05 14:50:55 +0100

    add tests for the actwidget

---
+++ cypress/integration/components/actwidget.spec.js
@@ -0,0 +1,32 @@
+import { isOn, skipOn } from '@cypress/skip-test';
+
+describe('ActWidget component', () => {
+    beforeEach(() => {
+        cy.visit('/act/honey');
+    });
+
+    it('generates mail-to-link with user data [dynamic]', () => {
+        // note as this act widget loads the entries from the DB this might break in the future if honey requires other elements
+        cy.contains('label', 'Honey Gold balance');
+        cy.get('div[id="act-with-account"]').find('input[id="3-value-id_data"]').type('42');
+        cy.get('div[id="act-with-account"]').contains('Send email').click();
+        cy.get('div[id="act-with-account"]')
+            .contains('Default email software')
+            .should('have.attr', 'href')
+            .and('match', /^mailto:[\s\S]*?balance[\s\S]*?42[\s\S]*?$/);
+        cy.get('div[id="act-with-account"]').contains('Default email software').clickLinkWithoutFollowingHref();
+    });
+
+    it('generates mail-to-link with user data [static]', () => {
+        cy.contains('label', 'userId');
+        cy.contains('label', 'deviceId');
+        cy.get('div[id="act-no-account"]').find('input[id="3-value-id_data"]').type('132465789ACME');
+        cy.get('div[id="act-no-account"]').find('input[id="4-value-id_data"]').type('987654321ACME');
+        cy.get('div[id="act-no-account"]').contains('Send email').click();
+        cy.get('div[id="act-no-account"]')
+            .contains('Default email software')
+            .should('have.attr', 'href')
+            .and('match', /^mailto:[\s\S]*?userId[\s\S]*?132465789ACME[\s\S]*?deviceId[\s\S]*?987654321ACME[\s\S]*?$/);
+        cy.get('div[id="act-no-account"]').contains('Default email software').clickLinkWithoutFollowingHref();
+    });
+});
