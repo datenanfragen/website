81d53166a2c6c61fb743d981ccaf24652951ad94
Benjamin Altpeter <benjamin@oldpeter.de>
2020-12-03 15:26:55 +0100

    DonationWidget: Disable credit card payments for now
    Mollie disabled them on our account due to potential fraudulent
    transactions by people testing stolen credit cards.

--- cypress/integration/old-bugs/donation-widget-nan.js
+++ cypress/integration/old-bugs/donation-widget-nan.js
@@ -14,49 +14,33 @@ describe('Donation widget amount', () => {
     });
 
     it('Amounts less than 1€ should only be allowed for bank transfers', () => {
-        cy.get('#donation-widget-amount')
-            .type('{selectall}0{uparrow}{uparrow}{uparrow}{uparrow}')
-            .trigger('input');
+        cy.get('#donation-widget-amount').type('{selectall}0{uparrow}{uparrow}{uparrow}{uparrow}').trigger('input');
         cy.contains('Bank transfer').click();
         cy.contains('To the next step').click();
-        cy.get('#bank-transfer-data-table')
-            .contains('Amount')
-            .siblings()
-            .last()
-            .contains('0.04');
+        cy.get('#bank-transfer-data-table').contains('Amount').siblings().last().contains('0.04');
 
         cy.get('#donation-widget-back-button').click();
 
-        const testMethod = method => {
+        const testMethod = (method) => {
             cy.contains(method).click();
             cy.contains('To the next step').click();
-            cy.get('.flash-message.flash-error')
-                .contains('less than')
-                .parent()
-                .get('button.close-button')
-                .click();
+            cy.get('.flash-message.flash-error').contains('less than').parent().get('button.close-button').click();
             // Make sure the flash has actually been closed before proceeding.
             cy.get('.flash-message.flash-error').should('not.exist');
         };
-        testMethod('Credit card');
+        // testMethod('Credit card');
         testMethod('Crypto currency');
         testMethod('PayPal');
         testMethod('Other payments');
     });
 
     it('Donation of negative values via bank transfer should not be allowed', () => {
-        cy.get('#donation-widget-amount')
-            .type('{selectall}5{leftarrow}-')
-            .trigger('input');
+        cy.get('#donation-widget-amount').type('{selectall}5{leftarrow}-').trigger('input');
 
-        const testMethod = method => {
+        const testMethod = (method) => {
             cy.contains(method).click();
             cy.contains('To the next step').click();
-            cy.get('.flash-message.flash-error')
-                .contains('valid amount')
-                .parent()
-                .get('button.close-button')
-                .click();
+            cy.get('.flash-message.flash-error').contains('valid amount').parent().get('button.close-button').click();
             // Make sure the flash has actually been closed before proceeding.
             cy.get('.flash-message.flash-error').should('not.exist');
         };
