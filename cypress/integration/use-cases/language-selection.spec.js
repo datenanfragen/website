bf3e67bd6daf5a9084f7b6774f70094b18c37c5c
mal-tee <45886910+mal-tee@users.noreply.github.com>
2021-05-03 16:52:49 +0200

    test if 'Change language' link is correct

--- cypress/integration/use-cases/language-selection.spec.js
+++ cypress/integration/use-cases/language-selection.spec.js
@@ -2,8 +2,13 @@ describe('switching languages', () => {
     it('shows a warning', () => {
         cy.visit('/');
         cy.contains('English /');
-        cy.get('footer .i18n-widget-language select').select('German (Deutsch)', { force: true });
+        cy.get('footer .i18n-widget-language select').select('German (Deutsch)', {
+            force: true,
+        });
         cy.contains('Changing the language will redirect you to another domain.');
-        cy.contains('Change language');
+        // RegEx taken from https://github.com/datenanfragen/website/pull/564/files#diff-56682af89782eee8f291dc44df0b21449b4203c2bc27990c869d3cd83ba8e036R9
+        cy.contains('Change language')
+            .should('have.attr', 'href')
+            .and('match', /`${Cypress.env('baseUrl_DE') || Cypress.config().baseUrl.replace('1314', '1313')}`/);
     });
 });
