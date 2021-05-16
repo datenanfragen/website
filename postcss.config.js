c9ea159d15e3a76b32683ad526918e1ea6c2bca7
mal-tee <45886910+mal-tee@users.noreply.github.com>
2021-05-16 16:58:27 +0200

    `postcss-url`: Don't inline every image
    * The config was broken before.
    * This is especially useful for the bg-wave on the homepage.
      Now it's in a separate file, which saves us some KB in the main.css.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 
+/*
+ * We want to copy the file in a different path than the one we reference in the generated file. Thats why we need to use a custom function for postcss-url. We don't want to duplicate code/functionality, therefore we include the default function.
+ */
+const processCopy = require('postcss-url/src/type/copy');
+
 module.exports = (ctx) => {
     return {
         plugins: [
@@ -13,18 +18,14 @@ module.exports = (ctx) => {
             require('cssnano')({}),
             require('postcss-url')([
                 {
-                    filter: /\.svg/,
+                    filter: /\.(svg|png|jpg|gif|eot|ttf|woff|woff2)$/,
                     basePath: path.resolve(__dirname, 'assets/'),
-                    url: 'inline',
+                    maxSize: 5,
+                    url: (...args) => '../' + processCopy(...args).replace('static/', ''),
+                    // we copy the files in a subfolder of static, we can't copy in to static directly (we would have to .gitignore them)
+                    // sadly we can't copy into static/img either :(
+                    assetsPath: path.resolve(__dirname, 'static/gen'),
                     optimizeSvgEncode: true,
-                },
-                {
-                    filter: /\.(png|jpg|gif|eot|ttf|woff|woff2)$/,
-                    basePath: path.resolve(__dirname, 'assets/'),
-                    maxSize: 10,
-                    fallback: 'copy',
-                    assetsPath: path.resolve(__dirname, 'public/img'),
-                    useHash: true,
                 },
             ]),
         ],
