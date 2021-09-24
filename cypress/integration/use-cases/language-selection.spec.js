79be218771d10dc48b68f86a99ba11d0923cbd5d
David Newham <davidpaulnewham@gmail.com>
2021-09-24 08:32:50 +0100

    Update language-selection.spec.js

--- cypress/integration/use-cases/language-selection.spec.js
+++ cypress/integration/use-cases/language-selection.spec.js
@@ -5,9 +5,9 @@ describe('switching languages', () => {
         cy.get('footer .i18n-widget-language select').select('German (Deutsch)', {
             force: true,
         });
-        cy.contains('Changing the language will redirect you to another domain.');
+        cy.contains('Wenn Du die Sprache wechelst, wirst Du zu einer anderen Domain weitergeleitet.');
         // RegEx taken from https://github.com/datenanfragen/website/pull/564/files#diff-56682af89782eee8f291dc44df0b21449b4203c2bc27990c869d3cd83ba8e036R9
-        cy.contains('Change language')
+        cy.contains('Trotzdem wechseln')
             .should('have.attr', 'href')
             .and('match', /`${Cypress.env('baseUrl_DE') || Cypress.config().baseUrl.replace('1314', '1313')}`/);
     });
