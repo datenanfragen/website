1811bb3bf76c4cf2c7d461ec193f34726b8c6bf8
David Newham <davidpaulnewham@gmail.com>
2021-10-03 11:48:58 +0100

    Sensible method for removing timeout -1

--- src/Components/FlashMessage.js
+++ src/Components/FlashMessage.js
@@ -5,9 +5,7 @@ import t from '../Utility/i18n';
 export default class FlashMessage extends Component {
     constructor(props) {
         super(props);
-
         if (!this.props.duration) this.props.duration = 5000;
-        if (this.props.duration === -1) this.props.duration = 2 ** 32 / 2 - 1;
         if (!this.props.type) this.props.type = 'info';
         this.state = {
             shown: true,
@@ -17,7 +15,9 @@ export default class FlashMessage extends Component {
     }
 
     componentDidMount() {
-        setTimeout(this.dismiss, this.props.duration);
+        if (this.props.duration !== -1) {
+            setTimeout(this.dismiss, this.props.duration);
+        }
     }
 
     dismiss() {
