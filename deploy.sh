55438fbe57744eae27adcf424019544a9969936f
Mohit Saxena <76725454+mohitsaxenaknoldus@users.noreply.github.com>
2021-10-07 15:48:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ then
     wget https://git.savannah.gnu.org/cgit/parallel.git/plain/src/parallel
     chmod 755 parallel
     cp parallel sem
-    sudo mv parallel sem /usr/bin/
+    mv parallel sem /usr/bin/
 fi
 
 # We don't need Netlify builds on master. This seems to be the easiest way to achieve this.
