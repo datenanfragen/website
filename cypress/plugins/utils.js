b1915011edebbb18dcdafa178f6baa6a9399faad
David Newham <davidpaulnewham@gmail.com>
2021-10-12 16:40:26 +0100

    Update utils.js

--- cypress/plugins/utils.js
+++ cypress/plugins/utils.js
@@ -1,17 +1,14 @@
 const { rmdir } = require('fs');
 
 const deleteFolder = (folderName) => {
-    /* eslint-disable no-console */
     return new Promise((resolve, reject) => {
         rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
             if (err) {
-                console.error(err);
                 return reject(err);
             }
             resolve(null);
         });
     });
-    /* eslint-disable no-console */
 };
 
 module.exports = { deleteFolder };
