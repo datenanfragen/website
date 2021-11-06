8233c1805135e4b9fda3c716565d73bf9d52f2bf
Alyssa Holland <alysam0071@gmail.com>
2021-11-06 18:11:31 -0400

    Add logic to close details pop-up on outside click and remove underline

--- src/Components/Footnote.js
+++ src/Components/Footnote.js
@@ -1,8 +1,27 @@
 import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'preact/hooks';
 
 const Footnote = (props) => {
+    const detailsRef = useRef(null);
+
+    useEffect(() => {
+        //Adapted after: https://usehooks.com/useOnClickOutside/
+        const listener = (event) => {
+            if (detailsRef.current?.open && !detailsRef.current?.contains(event.target)) {
+                detailsRef.current.removeAttribute('open');
+            }
+        };
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, []);
+
     return (
-        <details className="footnote" id={props.id}>
+        <details className="footnote" id={props.id} ref={detailsRef}>
             <summary>
                 <sup>{props.index}</sup>
             </summary>
