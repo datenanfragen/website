datenanfragen/website

commit 184f5ceea0073b0f574f4ec271d64b12bced8f80

Fix bugs introduced by #776 (#835)

assets/styles/elements.scss
@@ -368,6 +368,10 @@ input[type='search'].aa-input-search {
         color: inherit;
     }
 
+    h4 {
+        margin: 0;
+    }
+
     a {
         margin: -12px;
         padding: 12px;
@@ -376,9 +380,12 @@ input[type='search'].aa-input-search {
         color: $text-color;
         display: inline-block;
 
-        &.anchor-overlay {
+        .anchor-overlay {
             position: absolute;
             margin: auto;
+            display: block;
+            width: 100%;
+            height: 100%;
             top: 0;
             right: 0;
             left: 0;

src/Components/SearchBar.js
@@ -80,7 +80,7 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                     (this.props.anchorize
                                         ? `<a class="no-link-decoration" href="${BASE_URL}company/${d.slug}">`
                                         : '') +
-                                    '<span class="anchor-overlay" aria-hidden="true"></span><span><h4>' +
+                                    '<div class="anchor-overlay" aria-hidden="true"></div><span><h4>' +
                                     (name_hs.length === 1 ? name_hs[0].snippet : d.name) +
                                     (d.quality === 'tested'
                                         ? '&nbsp;<span class="icon icon-check-badge color-green-800" title="' +
@@ -94,13 +94,13 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                     '</h4></span>' +
                                     (this.props.anchorize ? '</a>' : '') +
                                     (d.runs?.length
-                                        ? '<br><span>' +
+                                        ? '<span>' +
                                           t('also-runs', 'search') +
                                           (runs_hs.length === 1 ? runs_hs[0].snippets : d.runs).join(', ') +
                                           '</span>'
                                         : '') +
                                     (d.categories?.length
-                                        ? '<br><span>' +
+                                        ? '<span>' +
                                           t('categories', 'search') +
                                           d.categories.map((c) => t(c, 'categories')).join(', ') +
                                           '</span>'

commit 6fa59a523ceaccbf88ff8ad2d5da2de0436c3f60

Fix bugs introduced by #835 (closes #836)
- If a company had both runs entries and categories, the categories
  were not on a separate line.
- The icon hover text didn't work anymore due to the overlay (#836).
- I see no reason to wrap the h4 in an otherwise empty span.

assets/styles/elements.scss
@@ -370,6 +370,11 @@ input[type='search'].aa-input-search {
 
     h4 {
         margin: 0;
+
+        .icon {
+            position: relative;
+            z-index: 1;
+        }
     }
 
     a {

src/Components/SearchBar.js
@@ -80,7 +80,7 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                     (this.props.anchorize
                                         ? `<a class="no-link-decoration" href="${BASE_URL}company/${d.slug}">`
                                         : '') +
-                                    '<div class="anchor-overlay" aria-hidden="true"></div><span><h4>' +
+                                    '<div class="anchor-overlay" aria-hidden="true"></div><h4>' +
                                     (name_hs.length === 1 ? name_hs[0].snippet : d.name) +
                                     (d.quality === 'tested'
                                         ? '&nbsp;<span class="icon icon-check-badge color-green-800" title="' +
@@ -91,13 +91,13 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                           t('quality-unverified', 'search') +
                                           '"></span>'
                                         : '') +
-                                    '</h4></span>' +
+                                    '</h4>' +
                                     (this.props.anchorize ? '</a>' : '') +
                                     (d.runs?.length
                                         ? '<span>' +
                                           t('also-runs', 'search') +
                                           (runs_hs.length === 1 ? runs_hs[0].snippets : d.runs).join(', ') +
-                                          '</span>'
+                                          '</span><br>'
                                         : '') +
                                     (d.categories?.length
                                         ? '<span>' +