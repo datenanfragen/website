datenanfragen/website

commit aa8345cc1d9d78e1c4b6fbef8279ff1eaa774d67

Made links more accessible in Search Suggestions (#776)
* Enhanced the search suggestion links like requested

* Removed aria hidden on search results

* Changed strong to h4 for heading of search results

assets/styles/elements.scss
@@ -345,6 +345,7 @@ input[type='search'].aa-input-search {
     box-sizing: border-box;
 }
 .aa-suggestion {
+    position: relative;
     padding: 12px;
     cursor: pointer;
     & + .aa-suggestion {
@@ -374,6 +375,15 @@ input[type='search'].aa-input-search {
         height: 100%;
         color: $text-color;
         display: inline-block;
+
+        &.anchor-overlay {
+            position: absolute;
+            margin: auto;
+            top: 0;
+            right: 0;
+            left: 0;
+            bottom: 0;
+        }
     }
 }
 

src/Components/SearchBar.js
@@ -80,7 +80,7 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                     (this.props.anchorize
                                         ? `<a class="no-link-decoration" href="${BASE_URL}company/${d.slug}">`
                                         : '') +
-                                    '<span><strong>' +
+                                    '<span class="anchor-overlay" aria-hidden="true"></span><span><h4>' +
                                     (name_hs.length === 1 ? name_hs[0].snippet : d.name) +
                                     (d.quality === 'tested'
                                         ? '&nbsp;<span class="icon icon-check-badge color-green-800" title="' +
@@ -91,7 +91,8 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                           t('quality-unverified', 'search') +
                                           '"></span>'
                                         : '') +
-                                    '</strong></span>' +
+                                    '</h4></span>' +
+                                    (this.props.anchorize ? '</a>' : '') +
                                     (d.runs?.length
                                         ? '<br><span>' +
                                           t('also-runs', 'search') +
@@ -103,8 +104,7 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                           t('categories', 'search') +
                                           d.categories.map((c) => t(c, 'categories')).join(', ') +
                                           '</span>'
-                                        : '') +
-                                    (this.props.anchorize ? '</a>' : '')
+                                        : '')
                                 );
                             }),
                         empty:

commit ab239e293c1cef56b7f89d7a1a9a854b0dd65f48

Fix bugs introduced by #776

assets/styles/elements.scss
@@ -368,6 +368,10 @@ input[type='search'].aa-input-search {
         color: inherit;
     }
 
+    h4 {
+        margin: 0;
+    }
+
     a {
         margin: -12px;
         padding: 12px;
@@ -376,9 +380,12 @@ input[type='search'].aa-input-search {
         color: $text-color;
         display: inline-block;
 
-        &.anchor-overlay {
+        .anchor-overlay {
             position: absolute;
             margin: auto;
+            display: block;
+            width: 100%;
+            height: 100%;
             top: 0;
             right: 0;
             left: 0;

src/Components/SearchBar.js
@@ -80,7 +80,7 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                     (this.props.anchorize
                                         ? `<a class="no-link-decoration" href="${BASE_URL}company/${d.slug}">`
                                         : '') +
-                                    '<span class="anchor-overlay" aria-hidden="true"></span><span><h4>' +
+                                    '<div class="anchor-overlay" aria-hidden="true"></div><span><h4>' +
                                     (name_hs.length === 1 ? name_hs[0].snippet : d.name) +
                                     (d.quality === 'tested'
                                         ? '&nbsp;<span class="icon icon-check-badge color-green-800" title="' +
@@ -94,13 +94,13 @@ if (Privacy.isAllowed(PRIVACY_ACTIONS.SEARCH)) {
                                     '</h4></span>' +
                                     (this.props.anchorize ? '</a>' : '') +
                                     (d.runs?.length
-                                        ? '<br><span>' +
+                                        ? '<span>' +
                                           t('also-runs', 'search') +
                                           (runs_hs.length === 1 ? runs_hs[0].snippets : d.runs).join(', ') +
                                           '</span>'
                                         : '') +
                                     (d.categories?.length
-                                        ? '<br><span>' +
+                                        ? '<span>' +
                                           t('categories', 'search') +
                                           d.categories.map((c) => t(c, 'categories')).join(', ') +
                                           '</span>'