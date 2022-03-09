a23180ddbc0ab52408655b450984b84c00c0aad2
mal-tee <mal-tee@mailbox.org>
2022-03-09 16:46:29 +0100

    Upgrade typesense client to 1.2.2

--- src/Utility/search.js
+++ src/Utility/search.js
@@ -1,11 +1,13 @@
 import * as Typesense from 'typesense';
 
 export const searchClient = new Typesense.Client({
-    masterNode: {
-        host: 'search.datenanfragen.de',
-        port: '443',
-        protocol: 'https',
-        apiKey: '',
-    },
+    nodes: [
+        {
+            host: 'search.datenanfragen.de',
+            port: '443',
+            protocol: 'https',
+        },
+    ],
+    apiKey: '',
     timeoutSeconds: 2,
 });
