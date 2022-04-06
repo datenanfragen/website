1bed8f4b8e33e838030e55a8c8837c1b4bf49784
Lorenz Sieben <me@lorenz-sieben.com>
2022-04-06 22:19:55 +0200

    Use GeneratorStore as a context rather than a gloabl state

--- src/store/generator.ts
+++ src/store/generator.ts
@@ -1,9 +1,13 @@
 import create from 'zustand';
 import { RequestState, createRequestStore } from './request';
 import type { Request } from '../types/request';
+import createContext from 'zustand/context';
 
 type GeneratorState = RequestState<Request>;
 
-export const useGeneratorStore = create<GeneratorState>((set, get) => ({
-    ...createRequestStore(set, get),
-}));
+export const { Provider: RequestGeneratorProvider, useStore: useGeneratorStore } = createContext<GeneratorState>();
+
+export const createGeneratorStore = () =>
+    create<GeneratorState>((set, get) => ({
+        ...createRequestStore(set, get),
+    }));
