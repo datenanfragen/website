fd3f9cdbd4e9e6cd2d6ecabdbfe00198ab788adf
Lorenz Sieben <me@lorenz-sieben.com>
2022-04-06 22:16:19 +0200

    Convert TransportMediumChooser to Typescript

---
+++ src/Components/Generator/TransportMediumChooser.tsx
@@ -0,0 +1,35 @@
+import t from '../../Utility/i18n';
+import { Text, IntlProvider } from 'preact-i18n';
+import Radio from '../Radio';
+import { TransportMedium, TRANSPORT_MEDIA } from 'request';
+import type { JSX } from 'preact';
+
+type TransportMediumChooserProps = {
+    transportMedium: TransportMedium;
+    onChange: (value: TransportMedium) => void;
+};
+
+export default function TransportMediumChooser(props: TransportMediumChooserProps) {
+    return (
+        <IntlProvider scope="generator" definition={window.I18N_DEFINITION}>
+            <div className="request-transport-medium-chooser">
+                <Text id="request-transport-medium" />
+                <br />
+                <div className="radio-group">
+                    {TRANSPORT_MEDIA.map((transport_medium) => (
+                        <Radio
+                            id={`request-transport-medium-choice-${transport_medium}`}
+                            radio_variable={props.transportMedium}
+                            value={transport_medium}
+                            name="transport-medium"
+                            onChange={(e: JSX.TargetedEvent<HTMLInputElement, Event>) =>
+                                props.onChange((e.target as HTMLInputElement).value as TransportMedium)
+                            }
+                            label={t(transport_medium, 'generator')}
+                        />
+                    ))}
+                </div>
+            </div>
+        </IntlProvider>
+    );
+}
