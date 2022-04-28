22925a198393a0a734c997b60a1f84388c44be2c
Lorenz Sieben <me@lorenz-sieben.com>
2022-04-28 08:14:35 +0200

    Remove unused translation store

--- src/store/localization.ts
+++
@@ -1,9 +0,0 @@
-import type i18n_definition_type from '../i18n/en.json';
-
-type I18nLanguage = string;
-
-interface LocalizationState {
-    locale: string;
-    i18n_definition_requests: Record<I18nLanguage, typeof i18n_definition_type['requests']>;
-    i18n_definition: Omit<typeof i18n_definition_type, 'hugo'>;
-}
