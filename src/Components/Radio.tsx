3d81b1cd7c281da8d8ca1c6377d67859a6628f17
Lorenz Sieben <me@lorenz-sieben.com>
2022-05-24 22:28:11 +0200

    Add addon option to Radio

--- src/Components/Radio.tsx
+++ src/Components/Radio.tsx
@@ -7,19 +7,25 @@ type RadioProps = {
     value: string;
     name: string;
     onChange: (value: string) => void;
+    onClick?: (value: string) => void;
+    addon?: JSX.Element | string;
 };
 
 export const Radio = (props: RadioProps) => (
-    <label for={props.id} className={`radio-label${props.radioVariable === props.value ? ' active' : ''}`}>
-        <input
-            type="radio"
-            id={props.id}
-            name={props.name}
-            value={props.value}
-            className="form-element"
-            checked={props.radioVariable === props.value}
-            onChange={(e) => props.onChange(e.currentTarget.value)}
-        />
-        {props.label}
-    </label>
+    <div className="radio-wrapper">
+        <label for={props.id} className={`radio-label${props.radioVariable === props.value ? ' active' : ''}`}>
+            <input
+                type="radio"
+                id={props.id}
+                name={props.name}
+                value={props.value}
+                className="form-element"
+                checked={props.radioVariable === props.value}
+                onChange={(e) => props.onChange(e.currentTarget.value)}
+                onClick={(e) => props.onClick?.(e.currentTarget.value)}
+            />
+            {props.label}
+        </label>
+        {props.addon && <div className="radio-addon">{props.addon}</div>}
+    </div>
 );
