5a68b7d2e77aa5de05483cde4310ddc741180bc2
Lorenz Sieben <me@lorenz-sieben.com>
2022-06-01 12:21:24 +0200

    Try at fixing flaky search tests

--- cypress/integration/company-database/search.spec.js
+++ cypress/integration/company-database/search.spec.js
@@ -4,8 +4,9 @@ describe('Search for company by slug and visit company detail page', () => {
     });
 
     it('Search for and visit Facebook single page', () => {
-        cy.get('#aa-search-input').type('facebook');
-        cy.contains('Facebook Ireland Ltd.').click();
+        cy.get('#aa-search-input').type('facebook').blur();
+        // The blur should fix the flaking here: https://github.com/cypress-io/cypress/issues/5830
+        cy.get('.aa-suggestions').contains('Facebook Ireland Ltd.').click();
 
         cy.url().should('include', '/company/facebook');
         cy.title().should('include', 'Facebook Ireland Ltd.');
