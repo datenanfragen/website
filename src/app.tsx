6bd3feb34e60c23f01828417a278e1d4bcde1c56
Lorenz Sieben <me@lorenz-sieben.com>
2022-06-02 17:53:30 +0200

    Simpler development with debug cookies

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,19 @@
 import { render } from 'preact';
 import { createGeneratorStore, RequestGeneratorProvider } from './store/generator';
-import { App } from './Components/App/App';
+import { App, AppPageId } from './Components/App/App';
+import Cookie from 'js-cookie';
 
 const elem = document.querySelector('main');
 if (elem) {
     render(
         <RequestGeneratorProvider createStore={createGeneratorStore}>
-            <App />
+            <App
+                initialPageId={
+                    process.env.NODE_ENV === 'development'
+                        ? (Cookie.get('DEBUG_INITAL_PAGE_ID') as AppPageId)
+                        : undefined
+                }
+            />
         </RequestGeneratorProvider>,
         elem
     );
