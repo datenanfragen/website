33a665d24a6cb96f3d177f62d3743796e36bc115
Lorenz Sieben <me@lorenz-sieben.com>
2022-06-22 15:30:22 +0200

    Add Breadcrumbs to the input stage

--- src/Components/App/FillRequestsPage.tsx
+++ src/Components/App/FillRequestsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'preact/hooks';
-import { MarkupText } from 'preact-i18n';
+import { MarkupText, Text } from 'preact-i18n';
 import { useGeneratorStore } from '../../store/generator';
 import { SetPageFunction } from './App';
 import { StatefulDynamicInputContainer } from '../Generator/DynamicInputContainer';
@@ -23,6 +23,7 @@ export const FillRequestsPage = (props: FillRequestsPageProps) => {
 
     return (
         <>
+            <BatchBreadcrumbs />
             <h2>{current_company.name}</h2>
             {/* TODO: Better explanation. */}
             <MarkupText id="id-data-explanation" />
@@ -31,5 +32,65 @@ export const FillRequestsPage = (props: FillRequestsPageProps) => {
             {/* TODO: Don't we need to call `renderLetter()`? (From a quick investigation: Emails don't need it (even though it does something for emails…) but PDFs do.) */}
             <SendRequestButton />
         </>
+    );
+};
+
+const BatchBreadcrumbs = () => {
+    const [batch, current_company] = useGeneratorStore((state) => [state.batch, state.current_company]);
+    const batch_length = Object.keys(batch || {}).length || 0;
+    const completed_requests = Object.values(batch || {}).filter((batchEntry) => batchEntry.done);
+    const completed_request_num = completed_requests?.length || 0;
+    const remaining_requests_num = batch_length - completed_request_num;
+    const current_company_index = Object.keys(batch || {}).findIndex((item) => item === current_company?.slug);
+
+    return (
+        <nav>
+            <ol className="batch-breadcrumbs">
+                {completed_request_num >= 1 && (
+                    <li className="batch-breadcrumb">
+                        <span className="batch-breadcrumb-sent">
+                            <Text
+                                id="batch-breadcrumbs-sent"
+                                plural={completed_request_num}
+                                fields={{ sent: completed_request_num }}
+                            />
+                        </span>
+                    </li>
+                )}
+                {current_company && (
+                    <li className="batch-breadcrumb" aria-current="page">
+                        <span className="batch-breadcrumb-active">{current_company.name}</span>
+                    </li>
+                )}
+                {batch && current_company_index + 1 < batch_length && (
+                    <li className="batch-breadcrumb desktop-only">
+                        <span>{Object.entries(batch)[current_company_index + 1][1].company.name}</span>
+                    </li>
+                )}
+                {remaining_requests_num >= 2 && (
+                    <li className="batch-breadcrumb desktop-only">
+                        <span>
+                            <Text
+                                id="batch-breadcrumbs-remaining"
+                                plural={remaining_requests_num - 1}
+                                fields={{ left: remaining_requests_num - 1 }}
+                            />
+                        </span>
+                    </li>
+                )}
+                {/* We do this to save space on mobile and hide the upcoming request, but then the request count is off, so we have it here twice. */}
+                {remaining_requests_num > 1 && (
+                    <li className="batch-breadcrumb mobile-only">
+                        <span>
+                            <Text
+                                id="batch-breadcrumbs-remaining"
+                                plural={remaining_requests_num - 1}
+                                fields={{ left: remaining_requests_num - 1 }}
+                            />
+                        </span>
+                    </li>
+                )}
+            </ol>
+        </nav>
     );
 };
