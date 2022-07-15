3e2531420af913886783148f5669dd7b36da8574
Benjamin Altpeter <benjamin@oldpeter.de>
2022-07-15 17:34:00 +0200

    Make most <Radio> props optional

--- src/Components/Radio.tsx
+++ src/Components/Radio.tsx
@@ -1,19 +1,21 @@
 import type { ComponentChildren } from 'preact';
 
 type RadioProps = {
-    id: string;
+    id?: string;
     label: ComponentChildren;
-    radioVariable: string;
-    value: string;
-    name: string;
-    onChange: (value: string) => void;
+    radioVariable?: string;
+    value?: string;
+    name?: string;
+    onChange?: (value: string) => void;
     onClick?: (value: string) => void;
     addon?: ComponentChildren;
 };
 
 export const Radio = (props: RadioProps) => (
     <div className="radio-wrapper">
-        <label for={props.id} className={`radio-label${props.radioVariable === props.value ? ' active' : ''}`}>
+        <label
+            for={props.id}
+            className={`radio-label${props.radioVariable && props.radioVariable === props.value ? ' active' : ''}`}>
             <input
                 type="radio"
                 id={props.id}
@@ -21,7 +23,7 @@ export const Radio = (props: RadioProps) => (
                 value={props.value}
                 className="form-element"
                 checked={props.radioVariable === props.value}
-                onChange={(e) => props.onChange(e.currentTarget.value)}
+                onChange={(e) => props.onChange?.(e.currentTarget.value)}
                 onClick={(e) => props.onClick?.(e.currentTarget.value)}
             />
             {props.label}
