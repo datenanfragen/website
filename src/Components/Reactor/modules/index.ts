d39f0d6bc93798c76dda9f2beb5b4d65acf43aca
Benjamin Altpeter <benjamin@oldpeter.de>
2022-07-21 15:16:51 +0200

    Reactor: Add 'company requires ID copy' case

--- src/Components/Reactor/modules/index.ts
+++ src/Components/Reactor/modules/index.ts
@@ -1,5 +1,6 @@
 import { module as baseModule } from './base';
 import { module as additionalIdModule } from './additional-id';
+import { module as idCopyModule } from './id-copy';
 
-export const reactorModules = [baseModule, additionalIdModule] as const;
+export const reactorModules = [baseModule, additionalIdModule, idCopyModule] as const;
 export type ReactorModuleId = typeof reactorModules[number]['id'];
