e6ffe5c7ee97e845c1bab93a9630676b92779826
Benjamin Altpeter <benjamin@oldpeter.de>
2022-08-02 11:37:18 +0200

    Custom requests: Use ID data instead of custom sender address

--- src/Components/Generator/CustomRequestInput.tsx
+++ src/Components/Generator/CustomRequestInput.tsx
@@ -1,24 +1,17 @@
 import { MarkupText, Text, IntlProvider } from 'preact-i18n';
 import t from '../../Utility/i18n';
 import { useGeneratorStore } from '../../store/generator';
-import { EMTPY_ADDRESS } from '../../Utility/requests';
-import { InputControl } from './DynamicInput';
 
 export const CustomRequestInput = () => {
     const request_type = useGeneratorStore((state) => state.request.type);
-    const name = useGeneratorStore((state) => (state.request.type === 'custom' ? state.request.custom_data.name : ''));
     const subject = useGeneratorStore((state) =>
         state.request.type === 'custom' ? state.request.custom_data.subject : ''
     );
     const content = useGeneratorStore((state) =>
         state.request.type === 'custom' ? state.request.custom_data.content : ''
     );
-    const sender_address = useGeneratorStore((state) =>
-        state.request.type === 'custom' ? state.request.custom_data.sender_address : EMTPY_ADDRESS
-    );
 
     const setCustomLetterProperty = useGeneratorStore((state) => state.setCustomLetterProperty);
-    const setCustomLetterAddress = useGeneratorStore((state) => state.setCustomLetterAddress);
 
     if (request_type !== 'custom') return null;
     return (
@@ -59,34 +52,6 @@ export const CustomRequestInput = () => {
                         <MarkupText id="tagxplanation" />
                     </div>
                 </div>
-            </div>
-            <div>
-                <h2 className="has-margin">
-                    <Text id="sender-address" />
-                </h2>
-                <div className="form-group fancy-fg">
-                    <input
-                        type="text"
-                        id="custom-sender-name"
-                        name="name"
-                        placeholder={t('name', 'generator')}
-                        className="form-element"
-                        onChange={(e) => setCustomLetterProperty('name', e.currentTarget.value)}
-                        value={name}
-                    />
-                    <label className="fancy-label" htmlFor="custom-sender-name">
-                        <Text id="name" />
-                    </label>
-                </div>
-
-                <InputControl
-                    type="address"
-                    id="0"
-                    suffix="custom-request"
-                    required={false}
-                    onChange={setCustomLetterAddress}
-                    value={sender_address}
-                />
             </div>
         </IntlProvider>
     );
