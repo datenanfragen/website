302cae34558519e949ff7ad65e7f6840a9f75504
Benjamin Altpeter <benjamin@oldpeter.de>
2022-08-24 17:18:44 +0200

    Move ProceedingsList to @datenanfragen/components

---
+++ components-package/src/App/Components/ProceedingsList.tsx
@@ -0,0 +1,40 @@
+import { RequestList } from '../../index';
+import { IntlProvider, Text } from 'preact-i18n';
+
+type ProceedingsListProps<PageId extends string> = {
+    setPage: (newPage: PageId) => void;
+};
+export const ProceedingsList = <PageId extends string>(props: ProceedingsListProps<PageId>) => (
+    <IntlProvider definition={window.I18N_DEFINITION_APP} scope="proceedings">
+        <header>
+            <h1>
+                <Text id="title" />
+            </h1>
+        </header>
+
+        <RequestList
+            emptyComponent={
+                <div className="box box-info" style="width: 80%; margin: auto;">
+                    <h2>
+                        <Text id="no-requests-heading" />
+                    </h2>
+                    <Text id="no-requests" />
+                    <br />
+
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a
+                        className="button button-primary"
+                        href=""
+                        style="float: right;"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            props.setPage('newRequests' as PageId);
+                        }}>
+                        <Text id="generate-request" />
+                    </a>
+                    <div className="clearfix" />
+                </div>
+            }
+        />
+    </IntlProvider>
+);
