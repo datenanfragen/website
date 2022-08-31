89be584eaadca70721e296dcf910b3133c9ffd76
Lorenz Sieben <me@lorenz-sieben.com>
2022-08-31 09:13:32 +0200

    Cherry-pick offline data compilation from new_ux

--- src/Utility/search.ts
+++ src/Utility/search.ts
@@ -11,3 +11,11 @@ export const searchClient = new Client({
     apiKey: '',
     connectionTimeoutSeconds: 2,
 });
+
+export const defaultSearchParams: any = {
+    query_by: 'name, runs, web, slug, address, comments',
+    sort_by: '_text_match:desc,sort-index:asc',
+    num_typos: 4,
+    per_page: 5,
+    snippet_threshold: 5,
+};
