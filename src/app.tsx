7523a1c3e28f2eec68e418839a80c80c84420f4a
Lorenz Sieben <me@lorenz-sieben.com>
2022-09-29 02:35:58 +0200

    Add simple tests

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,22 @@
 import { render } from 'preact';
-import { createGeneratorStore, RequestGeneratorProvider } from './store/generator';
+import { createGeneratorStore, RequestGeneratorProvider, useGeneratorStoreApi } from './store/generator';
 import { App } from './Components/App/App';
 import { Reactor } from './Components/Reactor/Reactor';
+import { useEffect } from 'preact/hooks';
+
+// Expose the store for the test interface
+const GeneratorStoreTestInterface = () => {
+    const generatorStoreApi = useGeneratorStoreApi();
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'development') {
+            (window as typeof window & { generatorStoreApi: typeof generatorStoreApi }).generatorStoreApi =
+                generatorStoreApi;
+        }
+    });
+
+    return <></>;
+};
 
 const elem = document.querySelector('main');
 if (elem) {
@@ -10,6 +25,7 @@ if (elem) {
             <Reactor reference={window.PARAMETERS.reference} />
         ) : (
             <RequestGeneratorProvider createStore={createGeneratorStore}>
+                {process.env.NODE_ENV === 'development' && <GeneratorStoreTestInterface />}
                 <App />
             </RequestGeneratorProvider>
         ),
