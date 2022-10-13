062bb8b8fdffc68f0f5cb148e479ef027fee2849
Benjamin Altpeter <benjamin@oldpeter.de>
2022-10-13 11:43:30 +0200

    Fix language selection warning test
    The I18nWidget now responds to the blur event.

--- cypress/integration/use-cases/language-selection.spec.js
+++ cypress/integration/use-cases/language-selection.spec.js
@@ -2,9 +2,9 @@ describe('switching languages', () => {
     it('shows a warning', () => {
         cy.visit('/');
         cy.contains('English /');
-        cy.get('footer .i18n-widget-language select').select('German (Deutsch)', {
-            force: true,
-        });
+        cy.get('footer').scrollIntoView();
+        cy.get('.i18n-button').click();
+        cy.get('footer .i18n-widget-language select').select('German (Deutsch)').blur();
         cy.contains('Wenn Du die Sprache wechelst, wirst Du zu einer anderen Domain weitergeleitet.');
         // RegEx taken from https://github.com/datenanfragen/website/pull/564/files#diff-56682af89782eee8f291dc44df0b21449b4203c2bc27990c869d3cd83ba8e036R9
         cy.contains('Trotzdem wechseln')
