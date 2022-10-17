38cec4db3c984aa675beb3e446abc8156ab2df32
Benjamin Altpeter <benjamin@oldpeter.de>
2022-10-17 17:50:05 +0200

    Tests: Get rid of Percy
    This has broken broken for ages. We may well want to try and re-enable
    Percy in the future, but then we'll have to start from scratch anyway.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,3 @@
-const percyHealthCheck = require('@percy/cypress/task');
-
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -13,8 +11,7 @@ const percyHealthCheck = require('@percy/cypress/task');
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
+module.exports = (/* on, config */) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
-    on('task', percyHealthCheck);
 };
