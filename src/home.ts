0de4647b3a0376e7566fb4e01ed48eb851bef0cf
Lorenz Sieben <me@lorenz-sieben.com>
2022-11-04 23:34:19 +0100

    Badge reminder for overdue proceedings

--- src/home.ts
+++ src/home.ts
@@ -1,6 +1,4 @@
-// import { render } from 'preact';
-import t from 'Utility/i18n';
-// import { ReminderWidget } from './Components/ReminderWidget';
+import t from './Utility/i18n';
 
 /* modified after https://codepen.io/danielgroen/pen/VeRPOq */
 const hero_rights = [
@@ -11,19 +9,16 @@ const hero_rights = [
     t('privacy', 'home'),
 ];
 
-// const reminderDiv = document.getElementById('home-reminders');
-// render(<ReminderWidget />, reminderDiv);
-
-function typewriter(text, i, fnCallback) {
+function typewriter(text: string, i: number, fnCallback: () => void) {
     if (text && i < text.length) {
-        document.getElementById('home-hero-word').textContent = text.substring(0, i + 1);
+        document.getElementById('home-hero-word')!.textContent = text.substring(0, i + 1);
 
         setTimeout(() => typewriter(text, i + 1, fnCallback), 150);
     } else if (typeof fnCallback == 'function') {
         setTimeout(fnCallback, 700);
     }
 }
-function startTextAnimation(i) {
+function startTextAnimation(i: number) {
     if (i < hero_rights.length) {
         typewriter(hero_rights[i], 0, () => {
             startTextAnimation(i + 1);
@@ -36,7 +31,7 @@ function startTextAnimation(i) {
 window.addEventListener('load', () => {
     const reducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
     if (reducedMotion.matches) {
-        document.getElementById('home-hero-word').textContent = hero_rights[hero_rights.length - 1];
+        document.getElementById('home-hero-word')!.textContent = hero_rights[hero_rights.length - 1];
     } else {
         startTextAnimation(0);
     }
