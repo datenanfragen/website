c5408136c03bb5eef102fcb2cb01b9a0a7eb8a44
Benjamin Altpeter <hi@bn.al>
2023-03-16 16:40:43 +0100

    Fixes #1044: Upgrade to Cypress 12

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    projectId: 'nug7wk',
+
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+
+    defaultCommandTimeout: 10000,
+    videoUploadOnPasses: false,
+
+    e2e: {
+        baseUrl: 'http://localhost:1314',
+
+        specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+
+        experimentalRunAllSpecs: true,
+        testIsolation: true,
+    },
+});
