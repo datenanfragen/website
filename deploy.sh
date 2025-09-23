2239fca613560a87289aac025480761fea5de1cd
Lorenz Sieben <me@lorenz-sieben.com>
2025-09-23 18:10:40 +0200

    Improve the deploy script using xargs

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+process_number=4
+
 # We don't need Netlify builds on master. This seems to be the easiest way to achieve this.
 # See https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables for the env vars set by Netlify.
 if [ "$NETLIFY" = "true" ] && [ "$BRANCH" = "master" ];
@@ -16,6 +18,8 @@ mkdir -p i18n
 languages=(de en fr pt es hr nl cs)
 
 echo "Fetching data…"
+# If the previous deploy script failed, we need to remove the data_tmp folder first,
+rm -rf data_tmp
 git clone --depth 1 https://github.com/datenanfragen/data data_tmp
 
 # When developing locally, we need to make sure to remove any old company pages.
@@ -24,11 +28,10 @@ rm -rf content/**/supervisory-authority
 rm -rf static/db
 
 echo "Creating directories…"
-for lang in ${languages[@]}
-do
-    mkdir -p "content/$lang/company"
-    mkdir -p "content/$lang/supervisory-authority"
-done
+company_content_dir="$(printf "content/%s/company\n" "${languages[@]}")"
+sva_content_dir=$(printf "content/%s/supervisory-authority\n" "${languages[@]}")
+
+echo -e "$company_content_dir\n$sva_content_dir" | xargs -n 1 -P $process_number mkdir -p
 
 mkdir -p static/templates
 mkdir -p static/db
@@ -43,14 +46,12 @@ cp data_tmp/supervisory-authorities/* static/db/sva
 # Since we're spawing a shell for each file, this is fairly expensive and considering we have thousands of company
 # records, it really adds up. To speed this up, we rename all files once in `data_tmp` and only _then_ copy them to the
 # language directories.
-find "data_tmp/companies" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
-find "data_tmp/supervisory-authorities" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
+find "data_tmp" -regex '.*/\(companies\|supervisory-authorities\)/.*\.json$' -print | sed -e "p" -e "s/.json$/.md/g" | xargs -P $process_number -n 2  mv
 
-for lang in ${languages[@]}
-do
-    cp data_tmp/companies/* "content/$lang/company"
-    cp data_tmp/supervisory-authorities/* "content/$lang/supervisory-authority"
-done
+# We need to use the shell here because otherwise bash would expand the `*`, which crashes in Circle CI
+echo $company_content_dir | xargs -P $process_number -n 1 sh -c 'cp data_tmp/companies/* $0'
+echo $sva_content_dir | xargs -P $process_number -n 1 sh -c 'cp data_tmp/supervisory-authorities/* $0'
+
 
 cp -r data_tmp/templates/* static/templates
 
@@ -79,4 +80,4 @@ fi
 
 # Finds all generated css files, matches and removes the second last non-dot characters (the md5 hash) and renames the files to the new filename without hash
 # This is really not a good fix and I beg hugo to change this!
-find "public" -regex '.*/styles/.*\.css' -exec sh -c  'echo $0 | sed "s/\(.*\.min\)\.[^\.]*\(\.[^\.]*\)$/\1\2/" | xargs mv $0 ' {}  \;
+find "public" -regex '.*/styles/.*\.css' -print | sed -e "p" -e "s/\(.*\.min\)\.[^\.]*\(\.[^\.]*\)$/\1\2/" | xargs -P $process_number -n 2 mv
