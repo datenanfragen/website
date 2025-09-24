0b444b062626494f7017e2f4072911f0ff3d3ebf
Benjamin Altpeter <benjamin@oldpeter.de>
2025-09-24 11:40:15 +0200

    Fixes #674: Optimize deploy script by renaming and copying in parallel (#1189)
    Co-authored-by: Lorenz Sieben <me@lorenz-sieben.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+process_number=4
+
 # We don't need Netlify builds on master. This seems to be the easiest way to achieve this.
 # See https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables for the env vars set by Netlify.
 if [ "$NETLIFY" = "true" ] && [ "$BRANCH" = "master" ];
@@ -16,14 +18,20 @@ mkdir -p i18n
 languages=(de en fr pt es hr nl cs)
 
 echo "Fetching data…"
+# If the previous deploy script failed, we need to remove the data_tmp folder first,
+rm -rf data_tmp
 git clone --depth 1 https://github.com/datenanfragen/data data_tmp
 
+# When developing locally, we need to make sure to remove any old company pages.
+rm -rf content/**/company
+rm -rf content/**/supervisory-authority
+rm -rf static/db
+
 echo "Creating directories…"
-for lang in ${languages[@]}
-do
-    mkdir -p "content/$lang/company"
-    mkdir -p "content/$lang/supervisory-authority"
-done
+company_content_dir="$(printf "content/%s/company\n" "${languages[@]}")"
+sva_content_dir=$(printf "content/%s/supervisory-authority\n" "${languages[@]}")
+
+echo -e "$company_content_dir\n$sva_content_dir" | xargs -n 1 -P $process_number mkdir -p
 
 mkdir -p static/templates
 mkdir -p static/db
@@ -34,11 +42,13 @@ echo "Copying files…"
 cp data_tmp/companies/* static/db
 cp data_tmp/supervisory-authorities/* static/db/sva
 
-for lang in ${languages[@]}
-do
-    cp data_tmp/companies/* "content/$lang/company"
-    cp data_tmp/supervisory-authorities/* "content/$lang/supervisory-authority"
-done
+# Unfortunately, Hugo only accepts .md files as posts, so we have to rename our JSONs, see https://stackoverflow.com/a/27285610
+# To speed this up, we rename all files once in `data_tmp` and only then copy them to the language directories.
+find "data_tmp" -regex '.*/\(companies\|supervisory-authorities\)/.*\.json$' -print | sed -e "p" -e "s/.json$/.md/g" | xargs -P $process_number -n 2  mv
+
+# We need to use the shell here because otherwise bash would expand the `*`, which crashes in Circle CI
+echo $company_content_dir | xargs -P $process_number -n 1 sh -c 'cp data_tmp/companies/* $0'
+echo $sva_content_dir | xargs -P $process_number -n 1 sh -c 'cp data_tmp/supervisory-authorities/* $0'
 
 cp -r data_tmp/templates/* static/templates
 
@@ -50,16 +60,6 @@ yarn tsm scripts/compile-data-dump.ts
 rm -rf data_tmp
 
 cd content || exit
-
-# Unfortunately, Hugo only accepts .md files as posts, so we have to rename our JSONs, see https://stackoverflow.com/a/27285610
-echo "Renaming JSON files…"
-
-for lang in ${languages[@]}
-do
-    find "$lang/company" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
-    find "$lang/supervisory-authority" -name '*.json' -exec sh -c 'mv "$0" "${0%.json}.md"' {} \;
-done
-
 cd .. || exit
 
 yarn licenses generate-disclaimer --ignore-optional --ignore-platform > static/NOTICES.txt
@@ -77,4 +77,4 @@ fi
 
 # Finds all generated css files, matches and removes the second last non-dot characters (the md5 hash) and renames the files to the new filename without hash
 # This is really not a good fix and I beg hugo to change this!
-find "public" -regex '.*/styles/.*\.css' -exec sh -c  'echo $0 | sed "s/\(.*\.min\)\.[^\.]*\(\.[^\.]*\)$/\1\2/" | xargs mv $0 ' {}  \;
+find "public" -regex '.*/styles/.*\.css' -print | sed -e "p" -e "s/\(.*\.min\)\.[^\.]*\(\.[^\.]*\)$/\1\2/" | xargs -P $process_number -n 2 mv
