0b029cb68106807d9cd6b5cf93fc707cdab081e6
Malte <mal-tee@mailbox.org>
2026-04-13 21:09:29 +0200

    port SimilarList

--- src/suggest-edit.tsx
+++ src/suggest-edit.tsx
@@ -4,6 +4,9 @@ import { useFetch } from './hooks/useFetch';
 import { ajv } from './Utility/suggest';
 import { fetchCompanyDataBySlug } from './Utility/companies';
 import { useEffect, useState } from 'preact/hooks';
+import { searchClient } from './Utility/search';
+import { rethrow } from './Utility/errors';
+import t from './Utility/i18n';
 
 const pointersToHide = [
     '/slug',
@@ -21,6 +24,68 @@ const pointersToHide = [
     '/needs-id-document',
     '/nsfw',
 ];
+const SimilarList = (props: { name: string }) => {
+    const [similarMatches, setSimilarMatches] = useState<any>([]);
+
+    useEffect(() => {
+        const searchOptions = {
+            query_by: 'name, runs',
+            prefix: false,
+            sort_by: '_text_match:desc,sort-index:asc',
+            num_typos: 1,
+            per_page: 5,
+            drop_tokens_threshold: 0,
+            q: '',
+        };
+
+        if (props.name) {
+            searchOptions['q'] = props.name;
+            searchClient
+                .collections('companies')
+                .documents()
+                .search(searchOptions)
+                .then((res) =>
+                    setSimilarMatches(
+                        (res.hits &&
+                            res.hits.map((hit: any) => ({
+                                slug: hit.document.slug,
+                                name: hit.document.name,
+                                runs: hit.highlights
+                                    .filter((highlight: any) => highlight.field === 'runs')
+                                    .flatMap((highlight: any) => highlight.snippets),
+                            }))) ||
+                            []
+                    )
+                )
+                .catch((error) => rethrow(error));
+        }
+    }, [props.name]);
+
+    return (
+        similarMatches.length > 0 && (
+            <div className="similar-list">
+                <label>{t('similarly-named-companies', 'suggest')}</label>
+                <ul>
+                    {similarMatches.map((similarMatch: any) => (
+                        <li key={similarMatch.slug}>
+                            <a href={window.BASE_URL + 'company/' + similarMatch.slug} target="_blank" rel="noreferrer">
+                                {similarMatch.name}
+                            </a>
+                            {similarMatch.runs.length > 0 && (
+                                <>
+                                    {' '}
+                                    ({t('also-runs', 'suggest')}
+                                    {/* eslint-disable-next-line react/no-danger */}
+                                    <span dangerouslySetInnerHTML={{ __html: similarMatch.runs.join(', ') }} />)
+                                </>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    );
+};
 
 export const SuggestForm = () => {
     const [companyData, setCompanyData] = useState<any>(null);
@@ -51,6 +116,14 @@ export const SuggestForm = () => {
                 pointersToHide={pointersToHide}
                 initialData={companyData}
                 autoComplete="off"
+                helpers={[
+                    ({ value }) => ({
+                        pointers: ['/name'],
+                        type: 'custom-addon',
+                        hidden: !value,
+                        element: <SimilarList name={value as string} />,
+                    }),
+                ]}
             />
         );
     return <div>Loading company data...</div>;
